61fdb6ed1f8bdc172f5f20313117060fc3d1c213
qywang521 <361005601@qq.com>
2017-11-16 09:04:58 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/SpbootDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpbootDemoApplication.class, args);
+	}
+}
