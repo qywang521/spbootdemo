386299f2e6b45e46a0ef2c48520b59ac0640a724
qywang521 <361005601@qq.com>
2017-12-18 20:22:05 +0800

    1

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,8 +1,13 @@
 package com.example.demo.controller;
 
 
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -10,9 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @Value("${book.name}")
     private String name ;
+    private final Logger logger=Logger.getLogger(this.getClass());
 
-    @RequestMapping("/hello")
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping(value="/hello",method = RequestMethod.GET)
     public String index(){
+        ServiceInstance localServiceInstance = discoveryClient.getLocalServiceInstance();
+        logger.info("/hello,host:"+localServiceInstance.getHost()+",service_id:"+localServiceInstance.getServiceId());
         return "Hello spboot"+name;
     }
 }
